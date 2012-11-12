dd6d4b093a0cce91d00b4a9a769eeb84e802c06e
Nicola Orru <nigu.orru@gmail.com>
2012-11-12 19:16:25 +0000

    Added some Java code in comments for comparison

--- src/main/scala/net/itadinanta/scala/CallFromJava.scala
+++ src/main/scala/net/itadinanta/scala/CallFromJava.scala
@@ -1,5 +1,31 @@
 package net.itadinanta.scala
 
-class CallFromJava(val x: Int, val y: Int)
-
+class CallFromJava(val x: Int = 0, val y: Int = 0)
+/*
+ * 
+ * public class CallFromJava {
+ * 		private final Integer x;
+ * 		private final Integer y;
+ * 
+ * 		public CallFromJava() {
+ * 			this(new Integer(0), new Integer(0));
+ * 		}
+ * 
+ * 		public CallFromJava(Integer x) {
+ * 			this(x, new Integer(0));
+ * 		}
+ * 
+ * 		public CallFromJava(Integer x, Integer y) {
+ * 			this(x, y);
+ * 		}
+ * 
+ * 		public Integer x() {
+ * 			return this.x;
+ * 		}
+ * 
+ * 		public Integer y() {
+ * 			return this.y;
+ * 		}
+ * }
+ */
 class AnotherClassHere(val a: Int = 0, val b: Int = 0)
