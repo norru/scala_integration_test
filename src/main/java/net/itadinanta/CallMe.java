a8c6b1093e595e10393114d6436b9a05fedf608f
Nicola Orru <nigu.orru@gmail.com>
2012-11-12 17:13:41 +0000

    Initial commit

---
+++ src/main/java/net/itadinanta/CallMe.java
@@ -0,0 +1,8 @@
+package net.itadinanta;
+
+public class CallMe {
+	@Override
+	public String toString() {
+		return "world";
+	}
+}
---
+++ src/main/scala/samples/TestApp.scala
@@ -0,0 +1,7 @@
+package samples
+
+import net.itadinanta.CallMe
+
+object TestApp extends App {
+	println("Hello, " + new CallMe)
+}
